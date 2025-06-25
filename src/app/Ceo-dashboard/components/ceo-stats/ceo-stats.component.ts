40a9062d036b91eda8516bef3eef4da669b8e429
Dayron <Dayronbp06@gmail.com>
2025-06-25 11:51:04 -0400

    feat: add ceo dashboard

---
+++ src/app/Ceo-dashboard/components/ceo-stats/ceo-stats.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ceo-stats',
+  imports: [],
+  templateUrl: './ceo-stats.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CeoStatsComponent {}

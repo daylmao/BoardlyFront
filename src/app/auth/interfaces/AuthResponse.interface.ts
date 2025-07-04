f909a64414f35dac2b7758ba7315037d6b9a1785
Dayron <Dayronbp06@gmail.com>
2025-07-04 10:28:25 -0400

    update auth interfaces

--- src/app/auth/interfaces/AuthResponse.interface.ts
+++ src/app/auth/interfaces/AuthResponse.interface.ts
@@ -1,16 +1,6 @@
 export interface AuthResponse {
   isSuccess: boolean;
-  data: {
-    id: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    username: string;
-    roles: string[];
-    isVerified: boolean;
-    statusCodes: number;
-    jwtToken: string;
-    phoneNumber: string;
-  };
+
+  jwtToken: string;
   errorMessage: string | null;
 }

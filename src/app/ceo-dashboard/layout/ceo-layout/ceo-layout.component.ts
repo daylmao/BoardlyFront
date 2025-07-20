b50578338a8e7cfe18c79c424c093e53aa7dfe80
Dayron <Dayronbp06@gmail.com>
2025-07-20 17:48:15 -0400

    refactor: move nav to ceo layout

--- src/app/ceo-dashboard/layout/ceo-layout/ceo-layout.component.ts
+++ src/app/ceo-dashboard/layout/ceo-layout/ceo-layout.component.ts
@@ -1,10 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from '../../../shared/components/nav/nav.component';
+import { AuthService } from '../../../auth/services/AuthService.service';
 
 @Component({
   selector: 'app-ceo-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './ceo-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CeoLayoutComponent {}
+export class CeoLayoutComponent {
+  id = inject(AuthService).user()!.uid;
+}
